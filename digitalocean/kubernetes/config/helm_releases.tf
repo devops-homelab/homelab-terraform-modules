f3f4cfcc05af8cf59f21444a2dd33c2a2516f90a
njay4928 <navindu.jayatilake@sysco.com>
2025-02-08 15:15:13 +0530

    update

--- digitalocean/kubernetes/config/helm_releases.tf
+++ digitalocean/kubernetes/config/helm_releases.tf
@@ -10,7 +10,7 @@ resource "helm_release" "metrics-server" {
   namespace  = "kube-system"
   repository = "https://kubernetes-sigs.github.io/metrics-server/"
   version    = try(each.value.version, var.deploy_metrics-server.version, "")
-  values     = [file("${path.module}/values/metrics-server_values.yaml")]
+  values     = [file("${path.module}/helm_values/metrics_server_values.yaml")]
 
   dynamic "set" {
     for_each = try(each.value.additional_set, [])
