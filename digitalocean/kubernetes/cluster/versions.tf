fb4961de9b3761dab7ef3cd92676c5a858a0ed42
njay4928 <njay4928@sysco.com>
2025-08-24 00:26:22 +0530

    update Terraform provider version to 2.66.0 and remove required version constraint

--- digitalocean/kubernetes/cluster/versions.tf
+++ digitalocean/kubernetes/cluster/versions.tf
@@ -1,10 +1,9 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.4.6"
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = ">= 2.28.1"
+      version = "2.66.0"
     }
   }
 }
